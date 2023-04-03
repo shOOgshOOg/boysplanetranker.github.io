640e49bca40a2ec304a669a7beb5e27c2ab6c0f1
shOOgshOOg <100537588+shOOgshOOg@users.noreply.github.com>
2023-04-03 14:26:44 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -386,7 +386,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://noliuniverse.github.io/atlantisranker.github.io/";
+const currentURL = "https://shoogshoog.github.io/fantasyboysranker/";
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
